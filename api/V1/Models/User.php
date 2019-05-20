ed156454172c2be6d75a0f40dd109207ac0ef5cc
Luiz Gustavo Martins <contato@luizgustavoweb.com>
2019-05-20 10:04:08 -0300

    Remove authentication traits from user model

--- api/V1/Models/User.php
+++ api/V1/Models/User.php
@@ -1,14 +1,7 @@
 <?php
 namespace Api\V1\Models;
 
-use Illuminate\Auth\Authenticatable;
-use Laravel\Lumen\Auth\Authorizable;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-
-class User extends Base implements AuthenticatableContract, AuthorizableContract
+class User extends Base
 {
-    use Authenticatable, Authorizable;
-
     protected $hidden = ['api_token'];
 }
