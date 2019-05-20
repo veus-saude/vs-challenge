bacb22e623a0a9e3334b2796bc895352c8f3f29d
Luiz Gustavo Martins <contato@luizgustavoweb.com>
2019-05-20 10:59:45 -0300

    Server method for request helper

--- api/Helpers/Request.php
+++ api/Helpers/Request.php
@@ -3,15 +3,21 @@ namespace Api\Helpers;
 
 class Request
 {
-	protected $get;
+	protected $get, $server;
 
 	public function __construct()
 	{
 		$this->get = $_GET;
+		$this->server = $_SERVER;
 	}
 	
 	public function get($property, $fallbackValue = null)
 	{
 		return $this->get[$property] ?? $fallbackValue;
+	}
+	
+	public function server($property, $fallbackValue = null)
+	{
+		return $this->server[$property] ?? $fallbackValue;
 	}
 }
