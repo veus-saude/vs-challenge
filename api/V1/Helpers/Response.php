3fa3c4621af4b9ca3fcba774009c1f8f3c9175dd
Luiz Gustavo Martins <contato@luizgustavoweb.com>
2019-05-20 09:27:43 -0300

    Give status code when constructing Response

--- api/V1/Helpers/Response.php
+++ api/V1/Helpers/Response.php
@@ -3,12 +3,20 @@ namespace Api\V1\Helpers;
 
 class Response
 {
-	public function __construct() {}
+	public function __construct($statusCode = null)
+	{
+		if($statusCode) http_response_code($statusCode);
+	}
 	
 	public function json($content)
 	{
 		header('Content-Type: application/json');
 		
 		return json_encode($content, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+	}
+	
+	public function plain($content)
+	{
+		return $content;
 	}
 }
