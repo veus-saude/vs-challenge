9922de82ec5da0a9b5ba8c619b4a24fe98bbf982
Luiz Gustavo Martins <contato@luizgustavoweb.com>
2019-05-20 09:45:05 -0300

    Check if route file exists

--- index.php
+++ index.php
@@ -8,6 +8,16 @@ $dbService = new Illuminate\Database\Capsule\Manager;
 $dbService->addConnection(require('config/database.php'));
 $dbService->bootEloquent();
 
-list($version, $path) = explode('/', ltrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'));
+list($version, $path) = explode('/', trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') . '/');
 
-require 'routes/' . $version . '.php';
+if($version) {
+	$routePath = 'routes/' . $version . '.php';
+	
+	if(file_exists($routePath)) {
+		require $routePath;
+	} else {
+		echo (new Api\Helpers\Response(404))->plain('Versão não encontrada');
+	}
+} else {
+	echo (new Api\Helpers\Response(404))->plain('Versão não informada');
+}
