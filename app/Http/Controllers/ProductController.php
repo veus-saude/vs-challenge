d3fd1d0ec5cfa114d696f86b0741335a666063a1
Your Name <you@example.com>
2019-05-26 17:27:19 -0300

    Atualizar a ordem dos métodos no controller produto

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,27 +7,23 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function search(Request $request)
-    {
-        $productFilter = Product::getSearchPaginatorOrderby($request);
-
-        return response()->json([
-            'totalResults' => $productFilter["totalResults"],
-            'list' => $productFilter["list"]
-        ]);
-    }
-
     public function index()
     {
         return response()->json(Product::all());
     }
 
-    public function destroy(int $id)
+    public function store(Request $request)
+    {
+        $obj = new Product($request->all());
+        $obj->save();
+        return response()->json($obj,200);
+    }
+
+    public function update(Request $request, int $id)
     {
         $product = Product::find($id);
         if($product){
-            Product::destroy($id);
-            return Response()->json(true, 204);
+            return response()->json($product->update($request->all()),200);
         }else{
             $msg = 'Não existe o produto com id '.$id;
             return Response()->json($msg,400);
@@ -45,22 +41,26 @@ class ProductController extends Controller
         }
     }
 
-    public function update(Request $request, int $id)
+    public function destroy(int $id)
     {
         $product = Product::find($id);
         if($product){
-            return response()->json($product->update($request->all()),200);
+            Product::destroy($id);
+            return Response()->json(true, 204);
         }else{
             $msg = 'Não existe o produto com id '.$id;
             return Response()->json($msg,400);
         }
     }
 
-    public function store(Request $request)
+    public function search(Request $request)
     {
-        $obj = new Product($request->all());
-        $obj->save();
-        return response()->json($obj,200);
+        $productFilter = Product::getSearchPaginatorOrderby($request);
+
+        return response()->json([
+            'totalResults' => $productFilter["totalResults"],
+            'list' => $productFilter["list"]
+        ]);
     }
 
 }
