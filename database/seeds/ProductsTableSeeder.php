fd961cbd5f2c30996ff989db3828f281f387d4d6
Your Name <you@example.com>
2019-05-26 16:47:22 -0300

    foi adicionado os campos (preço e quantidade) na seeder products

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -18,22 +18,31 @@ class ProductsTableSeeder extends Seeder
             $products = new Product();
             $products->name = "seringa ".Str::random(5);
             $products->brand = 'BUNZL';
+            $products->price = 20.80;
+            $products->qtd = 30;
             $products->save();
 
             $products = new Product();
             $products->name = "seringa ".Str::random(5);
             $products->brand = 'XPOT';
+            $products->price = 17.35;
+            $products->qtd = 60;
             $products->save();
 
             $products = new Product();
             $products->name = "Agulha ".Str::random(5);
             $products->brand = 'BUNZL';
+            $products->price = 11.89;
+            $products->qtd = 80;
             $products->save();
 
             $products = new Product();
             $products->name = "Agulha ".Str::random(5);
             $products->brand = 'XPOT';
+            $products->price = 12.15;
+            $products->qtd = 97;
             $products->save();
+
         }
     }
 }
