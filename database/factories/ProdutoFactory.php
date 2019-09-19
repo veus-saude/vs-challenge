9058bc0c278d27fcd02a1ae564c8ac02caf1bb27
Felipe D. Teodoro <felipe.devops@gmail.com>
2019-09-18 23:44:11 -0300

    [ADD] Adicionado seed dos produtos

---
+++ database/factories/ProdutoFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+
+use App\Produto;
+use Faker\Generator as Faker;
+
+
+$factory->define(Produto::class, function (Faker $faker) {
+    return [
+        'nome'          => $faker->name,
+        'preco'         => $faker->randomFloat(2, 0,8),
+        'marca'         => $faker->name,
+        'quantidade'    => $faker->randomNumber()
+    ];
+});
