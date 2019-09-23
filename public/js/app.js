299c4b1308dc50cd93672fe595a53ce5004b3bf9
Felipe D. Teodoro <felipe.devops@gmail.com>
2019-09-23 12:44:37 -0300

    [ADD] Docker file

--- public/js/app.js
+++ public/js/app.js
@@ -52961,8 +52961,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/felipe/projetos/veus_saude_api/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/felipe/projetos/veus_saude_api/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/felipe/Projetos/veus_saude_api/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/felipe/Projetos/veus_saude_api/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
